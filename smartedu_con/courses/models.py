d84f4c1900805693be095df913dc961751d27c7a
Ali Eren Kayhan <a.erenkayhan@gmail.com>
2024-06-27 14:46:17 +0300

    Kurslarda taglar eklendi

--- smartedu_con/courses/models.py
+++ smartedu_con/courses/models.py
@@ -1,7 +1,23 @@
 from django.db import models
  
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
 class Course(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name='Kurs Adı', help_text="Kurs adını yazınız.")
+    category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
     description = models.TextField(blank=True, null=True, verbose_name='Açıklama', help_text="Kurs açıklamasını yazınız.")
     image = models.ImageField(upload_to='courses/%Y/%m/%d/', default="courses/default_courses_image.jpg", verbose_name='Kurs Resmi', help_text="Kurs resmini yükleyiniz.")
     date = models.DateTimeField(auto_now_add=True, verbose_name='Oluşturulma Tarihi', help_text="Kursun oluşturulma tarihini gösterir.")
